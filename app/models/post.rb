c3468b7949a2e3d37504bdee029af0dae4e7f5b1
sdefrancesco <skaterbastian@aol.com>
2016-07-01 19:21:29 -0400

    added paperclip and truncated some posts, also fixed home page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :postcardimg, :styles => { :medium => '300x300>', :thumb => '100x100#'}
+	has_attached_file :postcardimg, :styles => { :large => '800x800>', :medium => '300x300>', :thumb => '100x100#'}
 	validates_attachment_content_type :postcardimg, :content_type => /\Aimage\/.*\Z/
 end
